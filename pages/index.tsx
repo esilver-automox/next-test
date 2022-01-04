663653aba661a24dd51ca7b6624a7f5a5a3fcc58
esilver-automox <erica.silver@automox.com>
2022-01-04 10:06:22 -0700

    env

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+const NEXT_PUBLIC_HOSTNAME = process.env.NEXT_PUBLIC_HOSTNAME
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
+
+    <h3>This is {NEXT_PUBLIC_HOSTNAME}</h3>
     <p>
       <Link href="/about">
         <a>About</a>
